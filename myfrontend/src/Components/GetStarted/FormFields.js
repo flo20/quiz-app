0a9a166d4ab69730f62cd838cae22f6f2c042405
Florence Anipa <mawuaniipa@gmail.com>
2021-06-08 13:28:39 +0300

    Handling form submission

--- myfrontend/src/Components/GetStarted/FormFields.js
+++ myfrontend/src/Components/GetStarted/FormFields.js
@@ -8,20 +8,23 @@ const formik = useFormik({
         email:"",
         channel:""
     },
+    onSubmit: values=> {
+       console.log("Form data", values);
+    }
     
 })
-console.log(formik.values)
+//console.log(formik.values)
 
     return (
         <div>
-            <form className="input-container" style={{display:"flex", flexDirection:"column", width:"50%"}}>
+            <form onSubmit={formik.handleSubmit} className="input-container" style={{display:"flex", flexDirection:"column", width:"50%"}}>
                 <label htmlFor="name">Name</label>
                 <input type="text" name="name" id="name" onChange={formik.handleChange} value={formik.values.name}/>
                 <label htmlFor="name">Email</label>
                 <input type="email" name="email" id="email" onChange={formik.handleChange} value={formik.values.email}/>
                 <label htmlFor="channel">Channel</label>
                 <input type="text" name="channel" id="channel" onChange={formik.handleChange} value={formik.values.channel}/>
-                <button>Submit</button>
+                <button type="submit">Submit</button>
             </form>
         </div>
     )
